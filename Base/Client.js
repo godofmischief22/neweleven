b8f47bf9328819788ec0970f3d461e417cc5cda8
godofmischief22 <aadionline23@gmail.com>
2025-05-14 12:00:35 +0530

    Update Client.js

--- Base/Client.js
+++ Base/Client.js
@@ -157,7 +157,7 @@ class Main extends Client {
             setInterval(() => {
                 this.user.setPresence({
                     activities: [activities[i % activities.length]],
-                    status: "Playing", // change to "idle" or "dnd" if needed
+                    status: "idle", // change to "idle" or "dnd" if needed
                 });
                 i++;
             }, 15000); // every 15 seconds
